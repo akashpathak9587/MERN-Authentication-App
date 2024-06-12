dcf7091187cfc83eb11804be59d95881ad3e1bf8
Akash Pathak <akashpathak@Akashs-MacBook-Air.local>
2024-06-13 01:31:26 +0530

    some changes made to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,3 @@
-import {
-  Col,
-  Container,
-  Form,
-  FormControl,
-  FormGroup,
-  FormLabel,
-  Row,
-} from "react-bootstrap";
-import { RiErrorWarningFill, RiEyeLine, RiEyeOffLine } from "react-icons/ri";
-import Hero from "./assets/Sign-up/Hero.png";
-import Logo from "./assets/Sign-up/Logo.png";
-import Google from "./assets/Sign-up/Google.png";
-import { useState } from "react";
-import { Link } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import PasswordReset from "./components/PasswordReset";
